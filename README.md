d791323984d1d42d03570c8c8f33c51e91af27d3
karlos1000 <karlos.0@live.com.mx>
2023-04-11 18:15:31 -0600

    Carga inicial

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # ejercicio005
+Create a Vue component that displays a list of items on a web page,
+where each item is an object with a "name" property and an "age" property.
+The user must be able to filter the list by age using an age range.
+You must make use of Vue's directives v-for and v-bind to display each item in the list and
+v-model and v-on to handle the age range selected by the user.
 
 ## Project setup
 ```
---
+++ src/components/List.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div>
+            <label for="">Filtrar por edad: </label>
+            <input type="text" v-model="search">
+        </div>
+        <ul>
+            <template v-for="item in searchFn" v-bind:key="item.id">
+                <li>{{ item.name }} - {{ item.year }}</li>
+            </template>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+export default {
+    name: 'List',
+    props: {
+    },
+    setup() {
+        let search = ref('');
+        let obj = [
+            {
+                id: 1,
+                name: "Carlos",
+                year: 35
+            },
+            {
+                id: 2,
+                name: "Lus Maria",
+                year: 24
+            },
+            {
+                id: 3,
+                name: "Susana",
+                year: 30
+            }
+        ];
+
+        const searchFn = computed(() => {
+            if(search.value!=""){
+                return obj.filter(user => user.year==search.value );
+                // return obj.filter(usr => {
+                //     usr.year <= search.value;
+                // });
+            }else{
+                return obj;
+            }
+        })
+
+        return {
+            obj,
+            search,
+            searchFn
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
